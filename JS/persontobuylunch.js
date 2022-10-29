4f4c99460ce170daaad4f9441caa95938ad69c94
Yudhajit Chowdhury <110385709+itsyudhajit@users.noreply.github.com>
2022-10-29 14:38:10 +0530

    JavaScript Intermediate || Conditional Statements

---
+++ JS/persontobuylunch.js
@@ -0,0 +1,9 @@
+function whosPaying(names) {
+    
+    /******Don't change the code above*******/
+    var length = names.length;
+    var name = Math.floor(Math.random()*length);
+    return (names[name]+" is going to buy lunch today!");
+        
+    /******Don't change the code below*******/    
+    }
